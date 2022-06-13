9237da716342def6871d02f94ad88865f169d838
谢俊彦 <xiejunyan@talkmoney.cn>
2022-06-13 10:57:09 +0800

    为界面跳转加上动画效果

--- app/src/main/java/com/jeckonly/wanandroidcompose/MainActivity.kt
+++ app/src/main/java/com/jeckonly/wanandroidcompose/MainActivity.kt
@@ -3,16 +3,23 @@ package com.jeckonly.wanandroidcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
+import com.google.accompanist.navigation.material.ExperimentalMaterialNavigationApi
 import com.jeckonly.wanandroidcompose.ui.theme.WanandroidComposeTheme
 import com.ramcosta.composedestinations.DestinationsNavHost
+import com.ramcosta.composedestinations.animations.defaults.RootNavGraphDefaultAnimations
+import com.ramcosta.composedestinations.animations.rememberAnimatedNavHostEngine
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalAnimationApi::class, ExperimentalMaterialNavigationApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -22,7 +29,13 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    DestinationsNavHost(navGraph = NavGraphs.root)
+                    val engine = rememberAnimatedNavHostEngine(
+                        rootDefaultAnimations = RootNavGraphDefaultAnimations(
+                            enterTransition = { slideInHorizontally() },
+                            exitTransition = { slideOutHorizontally() }
+                        )
+                    )
+                    DestinationsNavHost(navGraph = NavGraphs.root, engine = engine)
                 }
             }
         }
